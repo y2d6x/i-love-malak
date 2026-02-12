26621407fa829878ea886487f78397390e0cc8e9
Yousif Ahmed <yousefstudy2006@gmail.com>
2026-02-13 00:37:22 +0300

    بابا

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import ClientLayout from "./ClientLayout";
 
 export const metadata: Metadata = {
-  title: " ملاك ❤️",
-  description: "مفاجأة ذكرى سنوية خاصة لملاك من عماد",
+  title: "ملاك",
+  description: "رسالة خاصة 💌",
   icons: {
     icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>❤️</text></svg>",
   },
